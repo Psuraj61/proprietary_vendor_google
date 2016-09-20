b2a800ba80fdf854b3ad613f3df41e7adf713fff
Harsh Shandilya <msfjarvis@gmail.com>
2016-09-20 12:35:12 +0530

    Underp everything
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- seedmtk/seedmtk-vendor.mk
+++ seedmtk/seedmtk-vendor.mk
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/google/sprout/sprout-vendor-blobs.mk)
+$(call inherit-product, vendor/google/seedmtk/seedmtk-vendor-blobs.mk)
